5efd5b695f263398982a23bfbf8d3739a94da569
EnderHunter15 <coatudinu15@gmail.com>
2025-05-14 15:17:57 +0300

    diet setup

--- src/app/diet/page.tsx
+++ src/app/diet/page.tsx
@@ -1,3 +1,18 @@
+"use client";
+import { AnimatedCircularProgressBar } from "@/components/ui/animated-circular-progress-bar";
+
 export default function dietPage() {
-  return <div>diet</div>;
+  const calories = 50;
+  return (
+    <div>
+      <h1>Diet Page</h1>
+      <AnimatedCircularProgressBar
+        value={calories}
+        min={0}
+        max={100}
+        gaugePrimaryColor="bg-primary"
+        gaugeSecondaryColor="text-muted-foreground"
+      />
+    </div>
+  );
 }
